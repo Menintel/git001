2098ae064fac69bbb52ba6cdea77a0e08f72a174
Menintel <menintel@tuta.io>
2025-01-24 17:01:38 +0100

    3rd RebaseProj002

--- exo001.cpp
+++ exo001.cpp
@@ -7,9 +7,6 @@ int main(){
     printf("This is from the cloned Repo: ");
     printf("First Collab on Machine One. ");
     printf("Second Collab on Machine Two.");
-<<<<<<< HEAD
     printf("Rebase in Proj001 Machine one");
-=======
     printf("2nd Rebase Proj002 Machine two");
->>>>>>> 8e5a06c (2nd RebaseProj002)
 }
